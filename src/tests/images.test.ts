8c595fd9c91f2ac6c2b9cc81149c0cd7b64ead15
Jens Putzeys <jens.putzeys@student.kuleuven.be>
2021-07-30 10:37:17 +0200

    create tests for images

---
+++ src/tests/images.test.ts
@@ -0,0 +1,33 @@
+import { describe, test, expect } from '@jest/globals';
+import * as aop from '../index';
+
+describe('Tests for images', () => {
+    test('Test image', () => {
+        const image = new aop.elements.Image(
+            'image1',
+            'url_source',
+            50,
+            45,
+            'alt_text',
+            'wrap_text',
+            45,
+            50,
+            'url',
+            30,
+            25,
+        );
+        const imageExpected = {
+            image1: 'url_source',
+            image1_max_width: 50,
+            image1_max_height: 45,
+            image1_alt_text: 'alt_text',
+            image1_wrap_text: 'wrap_text',
+            image1_rotation: 45,
+            image1_transparency: 50,
+            image1_url: 'url',
+            image1_width: 30,
+            image1_height: 25,
+        };
+        expect(image.asDict()).toEqual(imageExpected);
+    });
+});
