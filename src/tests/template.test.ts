a9dc7988d1f56433d3ae91e3302c96444a0280f1
Aakash Kc <aakashkcx@gmail.com>
2021-09-25 22:11:01 +0100

    add hash tests + replace files with dummy text

--- src/tests/template.test.ts
+++ src/tests/template.test.ts
@@ -4,54 +4,108 @@ import * as fs from 'fs/promises';
 import * as cop from '../index';
 
 describe('Tests for class Template', () => {
+    test('Test Template base64', () => {
+        const template = cop.Template.fromBase64('dummy', 'docx');
+        const templateExpected = {
+            file: 'dummy',
+            template_type: 'docx',
+        };
+        expect(template.templateDict()).toEqual(templateExpected);
+    });
+
     test('Test Template local file', async () => {
         const localPath = './data/tests/template.docx';
         const data = await fs.readFile(localPath);
-        const resource = cop.Template.fromLocalFile(localPath);
-        const resourceExpected = {
+        const template = cop.Template.fromLocalFile(localPath);
+        const templateExpected = {
             file: cop.ownUtils.rawToBase64(data),
             template_type: 'docx',
         };
-        expect(resource.templateDict()).toEqual(resourceExpected);
+        expect(template.templateDict()).toEqual(templateExpected);
     });
 
     test('Test Template delimiters', async () => {
-        const localPath = './data/tests/template.docx';
-        const data = await fs.readFile(localPath);
-        const resource = cop.Template.fromLocalFile(localPath, '<', '>');
-        const resourceExpected = {
-            file: cop.ownUtils.rawToBase64(data),
+        const template = cop.Template.fromBase64('dummy', 'docx', '<', '>');
+        const templateExpected = {
+            file: 'dummy',
             template_type: 'docx',
             start_delimiter: '<',
             end_delimiter: '>',
         };
-        expect(resource.templateDict()).toEqual(resourceExpected);
+        expect(template.templateDict()).toEqual(templateExpected);
     });
 
     test('Test Template hashing', async () => {
-        const localPath = './data/tests/template.docx';
-        const data = await fs.readFile(localPath);
-        const resource = cop.Template.fromLocalFile(localPath, '{', '}', true);
-        const resourceExpected = {
-            file: cop.ownUtils.rawToBase64(data),
+        const template = cop.Template.fromBase64(
+            'dummy',
+            'docx',
+            '{',
+            '}',
+            true,
+        );
+        const templateExpected = {
+            file: 'dummy',
             template_type: 'docx',
             should_hash: true,
             start_delimiter: '{',
             end_delimiter: '}',
         };
-        expect(resource.templateDict()).toEqual(resourceExpected);
+        expect(template.templateDict()).toEqual(templateExpected);
     });
 
-    test('Test Template updating hash', async () => {
-        const localPath = './data/tests/template.docx';
-        const resource = cop.Template.fromLocalFile(localPath, '{', '}', true);
-        resource.updateHash('test_hash');
-        const resourceExpected = {
+    test('Test Template with hash', async () => {
+        const template = cop.Template.fromBase64(
+            'dummy',
+            'docx',
+            '{',
+            '}',
+            false,
+            'test_hash',
+        );
+        const templateExpected = {
             template_type: 'docx',
             template_hash: 'test_hash',
             start_delimiter: '{',
             end_delimiter: '}',
         };
-        expect(resource.templateDict()).toEqual(resourceExpected);
+        expect(template.templateDict()).toEqual(templateExpected);
+    });
+
+    test('Test Template updating hash', async () => {
+        const template = cop.Template.fromBase64(
+            'dummy',
+            'docx',
+            '{',
+            '}',
+            true,
+        );
+        template.updateHash('test_hash');
+        const templateExpected = {
+            template_type: 'docx',
+            template_hash: 'test_hash',
+            start_delimiter: '{',
+            end_delimiter: '}',
+        };
+        expect(template.templateDict()).toEqual(templateExpected);
+    });
+
+    test('Test Template resetting hash', async () => {
+        const template = cop.Template.fromBase64(
+            'dummy',
+            'docx',
+            '{',
+            '}',
+            false,
+            'test_hash',
+        );
+        template.resetHash(true);
+        const templateExpected = {
+            file: 'dummy',
+            template_type: 'docx',
+            should_hash: true,
+            start_delimiter: '{',
+            end_delimiter: '}',
+        };
+        expect(template.templateDict()).toEqual(templateExpected);
     });
 });
