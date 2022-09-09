731eb80e3afd7e662fc9700a177df78e1e54716a
ram <ramchandra@arthasoftsolutions.com>
2022-09-09 16:30:48 +0545

    updated test for test for images

--- src/tests/images.test.ts
+++ src/tests/images.test.ts
@@ -15,6 +15,8 @@ describe('Tests for images', () => {
             'url',
             30,
             25,
+            true,
+            false
         );
         const imageExpected = {
             image1: 'url_source',
@@ -27,6 +29,8 @@ describe('Tests for images', () => {
             image1_url: 'url',
             image1_width: 30,
             image1_height: 25,
+            image1_maintain_aspect_ratio : true,
+            image1_ignore_error: false
         };
         expect(image.asDict()).toEqual(imageExpected);
     });
